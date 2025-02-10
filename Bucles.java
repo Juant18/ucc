a09d6b11a6c5c10cb3bb8c7e1d24f6a06dc7425c
Juant18 <juan.trivinor@campusucc.edu.co>
2025-02-10 14:59:00 +0000

    Bucles con for y while

---
+++ Bucles.java
@@ -0,0 +1,17 @@
+public class Bucles {
+    public static void main ( String [] args ) {
+    for (int i = 1; i <= 10; i ++) {
+    System.out.println (" Programacion orientada a objetos " ) ;
+}
+    System.out.println("------------------------");
+//While
+    int j =1 ;
+    while ( j<=5 ){
+    System.out.println(j+",: POO");
+    j++;
+    
+
+}
+}
+}
+     
