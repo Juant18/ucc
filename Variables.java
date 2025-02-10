ff80ca5de9583dcd35a20e49bbec8a4b46d38108
Juant18 <juan.trivinor@campusucc.edu.co>
2025-02-10 13:33:26 +0000

    Variables

---
+++ Variables.java
@@ -0,0 +1,12 @@
+public class Variables {
+    public static void main ( String [] args ) {
+    int edad = 25;
+    double altura = 1.75;
+    String nombre = " Juan Triviño ";
+    boolean estudiante = true ;
+    System . out . println (" Nombre : " + nombre + ", Edad : "
+    + edad + ",  Altura : " + altura + ",  Estudiante : "
+    + estudiante ) ;
+    }
+    }
+    
