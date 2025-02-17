75bddcb7209bb5811569f2b0ae3f2b056be16c8f
Juant18 <juan.trivinor@campusucc.edu.co>
2025-02-17 13:02:53 +0000

    Codigo para primera forma de arreglos

--- Arreglos.java
+++ Arreglos.java
@@ -1,16 +1,18 @@
 public class Arreglos{
     public static void main(String[] args) {
         //Como declarar arreglos primera forma
-        //int[] a = { 8, 3, 10, 22, 1, 0}
+        //int[] a = { 8, 3, 10, 22, 1, 0};
 //segunda forma
 //int[] a = new int[6] 
 //El numero hace referencia al tamaño, se tiene que llenar el arreglo
 
+ 
+
+int[] a = { 8, 3, 10, 22, 1, 9};
+  for(int i = 0; i < a.length; i++ ){
+   System.out.println("a["+ i +"]=" +a[i]);
 
 
-
-
-
-
+  }
     }
 }
