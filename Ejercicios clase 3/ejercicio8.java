139d168cf1b0a49a252e924eace671b862a7d0c1
Juant18 <juan.trivinor@campusucc.edu.co>
2025-02-22 20:16:05 -0500

    pide 30 numeros

---
+++ Ejercicios clase 3/ejercicio8.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class AnalizarNumerosSimple {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] numeros = new int[30];
+
+    
+        for (int i = 0; i < 30; i++) {
+            System.out.print("Número " + (i + 1) + ": ");
+            numeros[i] = scanner.nextInt();
+        }
+
+        int mayor = numeros[0];
+        int menor = numeros[0];
+        for (int num : numeros) {
+            if (num > mayor) mayor = num;
+            if (num < menor) menor = num;
+        }
+
+        int repMayor = 0, repMenor = 0;
+        for (int num : numeros) {
+            if (num == mayor) repMayor++;
+            if (num == menor) repMenor++;
+        }
+
+     
+        System.out.println("Mayor: " + mayor + " (x" + repMayor + ")");
+        System.out.println("Menor: " + menor + " (x" + repMenor + ")");
+
+        scanner.close();
+    }
+}
