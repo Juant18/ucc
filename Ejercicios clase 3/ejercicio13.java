a9a6fd2a0d2f4f6113e5ee48770c5a3118b5c39c
Juant18 <juan.trivinor@campusucc.edu.co>
2025-02-24 01:24:04 +0000

    ejercicio 13 de notas y promedio

---
+++ Ejercicios clase 3/ejercicio13.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList; 
+import java.util.Arrays; 
+
+public class SepararNumeros {
+
+    public static void main(String[] args) {
+     
+        int[] notas = {10, 5, 20, 15, 8, 3, 12};
+
+        double promedio = calcularPromedio(notas);
+
+       
+        ArrayList<Integer> mayores = new ArrayList<>();
+        ArrayList<Integer> menores = new ArrayList<>();
+
+      
+        for (int nota : notas) {
+            if (nota > promedio) {
+                mayores.add(nota); 
+            } else if (nota < promedio) {
+                menores.add(nota); 
+            }
+        }
+
+     
+        System.out.println("Notas: " + Arrays.toString(notas));
+        System.out.println("Promedio: " + promedio);
+        System.out.println("Notas mayores que el promedio: " + mayores);
+        System.out.println("Notas menores que el promedio: " + menores);
+    }
+
+
+    public static double calcularPromedio(int[] numeros) {
+        int suma = 0;
+        for (int numero : numeros) {
+            suma += numero;
+        }
+        return (double) suma / numeros.length;
+    }
+}
