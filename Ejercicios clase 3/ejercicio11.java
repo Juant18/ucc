3b6bc86dd49b8ab6c5646745d2d5d8112e674da8
Juant18 <juan.trivinor@campusucc.edu.co>
2025-02-23 20:27:04 -0500

    Update ejercicio11.java

--- Ejercicios clase 3/ejercicio11.java
+++ Ejercicios clase 3/ejercicio11.java
@@ -1,7 +1,7 @@
 
 import java.util.Arrays;
 
-public class SepararNumeros {
+public class ejercicio11 {
 
     public static void main(String[] args) {
         int[] arregloA = {1, -2, 0, 5, -8, 0, 3, -1, 7}; 
