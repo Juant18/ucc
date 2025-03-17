ad7927e16a589cf9bf5f2765b51c2a780b2757e9
Juant18 <juan.trivinor@campusucc.edu.co>
2025-03-17 14:08:08 +0000

    main codigo

---
+++ clasepoo/Metodomain.java
@@ -0,0 +1,10 @@
+
+
+public class Metodomain{
+ public static void main(String[] args) {
+    //instanciar el objeto creacion
+    Estudiante objEst1 = new Estudiante(1,"Sebastian", "Arias", 15,"ingieneria");
+    Estudiante objEst2 = new Estudiante1("Juan", "Triviño", 17,"derecho");
+
+}
+}
