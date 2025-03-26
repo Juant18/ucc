6394a9e142092429a463755f4ddca7ce08e4bd97
Juant18 <juan.trivinor@campusucc.edu.co>
2025-03-26 20:01:06 +0000

    se añadio contantes seno coseno tangente raiz y elevado

--- Calculadora.java
+++ Calculadora.java
@@ -41,16 +41,46 @@ public class Calculadora {
                 } else {
                     if (operacion == 4) {
                         resultado = num1 / num2;
-                    }
-                    if (num2 != 0) {
-                        resultado = num1 / num2;
                     } else {
-                        System.out.println("Error: División por 0 no permitida.");
+                        if (operacion == 5) {
+                            double raiz = Math.sqrt(num1);
+                            double raiz2 = Math.sqrt(num2);
+                            System.out.println("La raiz es " + raiz + " y la segunda es " + raiz2);
+
+                        } else {
+
+                            if (operacion == 6) {
+                                resultado = num1 * num1;
+                            } else {
+                                if (operacion == 7) {
+                                    double seno = Math.sin(Math.toRadians(num1));
+                                    double seno2 = Math.sin(Math.toRadians(num2));
+                                    System.out.println("La raiz es " + seno + " y la segunda es " + seno2);
+                                } else {
+                                    if (operacion == 8) {
+                                        double coseno = Math.cos(Math.toRadians(num1));
+                                        double coseno2 = Math.cos(Math.toRadians(num2));
+                                        System.out.println("La raiz es " + coseno + " y la segunda es " + coseno2);
+                                    } else {
+                                        if (operacion == 9) {
+                                            double tan = Math.tan(Math.toRadians(num1));
+                                            double tan2 = Math.tan(Math.toRadians(num2));
+                                            System.out.println("La raiz es " + tan + " y la segunda es " + tan2);
+                                        }
+                                    }
+
+                                }
+                            }
+                            if (num2 != 0) {
+                                resultado = num1 / num2;
+                            } else {
+                                System.out.println("Error: División por 0 no permitida.");
+                            }
+                        }
                     }
                 }
             }
         }
-
     }
 
     public static void main(String[] args) {
@@ -66,12 +96,18 @@ public class Calculadora {
             System.out.println("Seleccione 2 paa resta");
             System.out.println("Seleccione 3 para multiplicacion");
             System.out.println("Seleccione 4 para division");
-            System.out.println("Seleccione 5 para Salir");
+
+            System.out.println("Seleccione 5 para raiz");
+            System.out.println("Seleccione 6 para elevado");
+            System.out.println("Seleccione 7 para seno");
+            System.out.println("Seleccione 8 para coseno");
+            System.out.println("Seleccione 9 para tangente");
+            System.out.println("Seleccione 10 para Salir");
 
             opcion = leer.nextInt();
             leer.nextLine();
 
-            if (opcion == 5) {
+            if (opcion == 10) {
                 aceptar = false;
                 System.out.println("¡Hasta luego!");
                 break;
