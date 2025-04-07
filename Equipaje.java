91a94d63ff413adc29b1ecd3dbdab6176a9cc751
Juant18 <juan.trivinor@campusucc.edu.co>
2025-04-07 14:24:39 +0000

    actualizacion falla em main

---
+++ Equipaje.java
@@ -0,0 +1,120 @@
+class PrecioTotal {
+    private double totalPrecios = 0.0;
+    private double totalBodega = 0.0;
+    private double totalCabina = 0.0;
+    private Equipaje[] equipajes;
+
+    public PrecioTotal(Equipaje[] equipajes) {
+        this.equipajes = equipajes;
+    }
+
+    public void calcularTotales() {
+        for (Equipaje equipaje : equipajes) {
+            double precio = equipaje.calcularPrecio();
+            totalPrecios += precio;
+
+            if (equipaje instanceof Bodega) {
+                totalBodega += precio;
+            } else if (equipaje instanceof Cabina) {
+                totalCabina += precio;
+            }
+        }
+    }
+
+    public void mostrarTotales() {
+        // Calculo de totales
+        calcularTotales();
+        System.out.println("Total Equipaje: " + totalPrecios);
+        System.out.println("Total Bodega: " + totalBodega);
+        System.out.println("Total Cabina: " + totalCabina);
+    }
+}
+
+public class Equipaje {
+
+    private double PESO = 10.0;
+    private double TAMAÑO = 4.5;
+    private double PRECIO_BASE = 1000.0;
+  
+    protected double peso;
+    protected double tamaño;
+    protected double precioBase;
+
+    public Equipaje(double peso, double tamaño) {
+        this.peso = peso;
+        this.tamaño = tamaño;
+        this.precioBase = PRECIO_BASE;
+    }
+
+    public Equipaje(double precioBase) {
+        this.peso = PESO;
+        this.tamaño = TAMAÑO;
+        this.precioBase = precioBase;
+    }
+
+
+    public Equipaje() {
+        this.peso = PESO;
+        this.tamaño = TAMAÑO;
+        this.precioBase = PRECIO_BASE;
+    }
+
+    public double calcularPrecio() {
+        return precioBase;
+    }
+}
+
+class Bodega extends Equipaje {
+    private static final double CAPACIDAD = 5.0;
+
+    public Bodega(double peso, double tamaño) {
+        super(peso, tamaño);
+    }
+
+    public Bodega(double precioBase) {
+        super(precioBase);
+    }
+
+    public Bodega() {
+        super();
+    }
+
+    public double calcularPrecio() {
+        return precioBase + (peso * tamaño * CAPACIDAD);
+    }
+}
+
+// Clase Cabina que extiende de Equipaje
+class Cabina extends Equipaje {
+    private static final double TIEMPO = 2.0;
+
+    public Cabina(double peso, double tamaño) {
+        super(peso, tamaño);
+    }
+
+    public Cabina(double precioBase) {
+        super(precioBase);
+    }
+
+    public Cabina() {
+        super();
+    }
+
+    public double calcularPrecio() {
+        return precioBase + (peso * tamaño * TIEMPO);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Equipaje[] equipajes = new Equipaje[5];
+        equipajes[0] = new Bodega(100.0, 10.0);
+        equipajes[1] = new Bodega(200);
+        equipajes[2] = new Cabina(150, 20.0);
+        equipajes[3] = new Cabina();
+        equipajes[4] = new Bodega();
+
+        PrecioTotal solucion = new PrecioTotal(equipajes);
+        solucion.mostrarTotales();
+    }
+}
