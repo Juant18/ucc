40ed3323d6069ab0382b35acd24506724626e19b
Juant18 <juan.trivinor@campusucc.edu.co>
2025-05-05 12:48:09 +0000

    Clase converter

---
+++ Clase12/converter.java
@@ -0,0 +1,22 @@
+public class converter {
+    private String document;
+
+    public converter(String document) {
+        this.document = document;
+    }
+
+    public String getDocument(){
+        return document;
+    }
+
+    public void makeLine(){
+        System.out.println("");
+    }
+    public void makeParagraf(){
+        System.out.println("");
+
+    }
+    public void makeTable(){
+        System.out.println("");
+    }
+}
