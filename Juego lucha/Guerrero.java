27f55b5050c4ba0e869c3e4ad3eac306e53fa748
Juant18 <juan.trivinor@campusucc.edu.co>
2025-05-19 13:49:04 +0000

    creacion de armas

--- Juego lucha/Guerrero.java
+++ Juego lucha/Guerrero.java
@@ -1,6 +1,30 @@
-public class Guerrero extends Personaje {
-    // Constructor de la clase Guerrero
+
+class Guerrero extends Personaje {
+    private Random rand = new Random();
+
     public Guerrero(String nombre) {
-        super(nombre);  
+        super(nombre);
+    }
+
+    public void ataqueFuerte(Personaje oponente) {
+        int daño = rand.nextInt(30) + 15; // Ataque fuerte  de espada entre 15 y 44 de daño
+        oponente.recibirDaño(daño);
+        System.out.println(this.getNombre() + " usa ¡Ataque de espada! contra " + oponente.getNombre() + " causando " + daño + " puntos de daño.");
+    }
+
+    public void tajoRapido(Personaje oponente) {
+        int daño = rand.nextInt(15) + 5; // lanza ataque cuchilo entre 5 y 19 de daño
+        oponente.recibirDaño(daño);
+        System.out.println(this.getNombre() + " lanza un ¡Ataque de cuchillo! a " + oponente.getNombre() + " infligiendo " + daño + " puntos de daño.");
+    }
+
+
+    public void atacar(Personaje oponente) {
+        // El Guerrero tiene un 60% de usar ataque fuerte y 40% de usar tajo rápido
+        if (rand.nextDouble() < 0.6) {
+            ataqueFuerte(oponente);
+        } else {
+            tajoRapido(oponente);
+        }
     }
 }
--- Juego lucha/Mago.java
+++ Juego lucha/Mago.java
@@ -1,6 +1,29 @@
-public class Mago extends Personaje {
-    // Constructor de la clase Mago
+class Mago extends Personaje {
+    private Random rand = new Random();
+
     public Mago(String nombre) {
-        super(nombre); 
+        super(nombre);
+    }
+
+    public void bolaDeFuego(Personaje oponente) {
+        int daño = rand.nextInt(40) + 20; // Bola de fuego entre 20 y 59 de daño
+        oponente.recibirDaño(daño);
+        System.out.println(this.getNombre() + " lanza una ¡Bola de Fuego! a " + oponente.getNombre() + " quemando " + daño + " puntos de vida.");
+    }
+
+    public void rayoArcano(Personaje oponente) {
+        int daño = rand.nextInt(25) + 10; // Rayo arcano entre 10 y 34 de daño
+        oponente.recibirDaño(daño);
+        System.out.println(this.getNombre() + " conjura un ¡Rayo Arcano! impactando a " + oponente.getNombre() + " con " + daño + " puntos de daño mágico.");
+    }
+
+    @Override
+    public void atacar(Personaje oponente) {
+        // El Mago tiene un 50% de usar bola de fuego y 50% de usar rayo arcano
+        if (rand.nextDouble() < 0.5) {
+            bolaDeFuego(oponente);
+        } else {
+            rayoArcano(oponente);
+        }
     }
 }
